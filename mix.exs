f6742592daff8740679346507d8b9f7675f9cb53
Alessandro Mencarini <alessandro@driftrock.com>
2017-09-22 15:12:25 +0100

    Fix metadata

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule QuietLogger.Mixfile do
     [
       name: "quiet_logger",
       # These are the default files included in the package
-      files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
       maintainers: ["Alessandro Mencarini"],
       licenses: ["Apache 2.0"],
       links: %{"GitHub" => "https://github.com/Driftrock/quiet_logger"}
