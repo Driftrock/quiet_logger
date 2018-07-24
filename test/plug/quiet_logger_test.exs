274c040ea32ac41baa07ede1e81e81e752eb0a55
Alessandro Mencarini <alessandro@driftrock.com>
2018-07-24 13:45:27 +0100

    Apply mix formatter

--- test/plug/quiet_logger_test.exs
+++ test/plug/quiet_logger_test.exs
@@ -9,8 +9,8 @@ defmodule Plug.QuietLoggerTest do
   defmodule MyPlug do
     use Plug.Builder
 
-    plug Plug.QuietLogger
-    plug :passthrough
+    plug(Plug.QuietLogger)
+    plug(:passthrough)
 
     defp passthrough(conn, _) do
       Plug.Conn.send_resp(conn, 200, "Passthrough")
@@ -20,8 +20,8 @@ defmodule Plug.QuietLoggerTest do
   defmodule MyDebugPlug do
     use Plug.Builder
 
-    plug Plug.QuietLogger, log: :debug
-    plug :passthrough
+    plug(Plug.QuietLogger, log: :debug)
+    plug(:passthrough)
 
     defp passthrough(conn, _) do
       Plug.Conn.send_resp(conn, 200, "Passthrough")
@@ -31,20 +31,19 @@ defmodule Plug.QuietLoggerTest do
   defmodule MyCustomPathPlug do
     use Plug.Builder
 
-    plug Plug.QuietLogger, path: "/api/status"
-    plug :passthrough
+    plug(Plug.QuietLogger, path: "/api/status")
+    plug(:passthrough)
 
     defp passthrough(conn, _) do
       Plug.Conn.send_resp(conn, 200, "Passthrough")
     end
   end
 
-
   defmodule MyMultiPathPlug do
     use Plug.Builder
 
-    plug Plug.QuietLogger, path: ["/api/status", "/api/health"]
-    plug :passthrough
+    plug(Plug.QuietLogger, path: ["/api/status", "/api/health"])
+    plug(:passthrough)
 
     defp passthrough(conn, _) do
       Plug.Conn.send_resp(conn, 200, "Passthrough")
@@ -69,53 +68,63 @@ defmodule Plug.QuietLoggerTest do
 
   describe "Plug.QuietLogger.call/2" do
     test "it suppresses logging on the designated health check and log level" do
-      log = capture_log_lines fn ->
-        call(conn(:get, "/health-check"))
-      end
+      log =
+        capture_log_lines(fn ->
+          call(conn(:get, "/health-check"))
+        end)
 
       assert log == ""
     end
 
     test "it allows logging on other endpoints" do
-      log = capture_log_lines fn ->
-        call(conn(:get, "/"))
-      end
+      log =
+        capture_log_lines(fn ->
+          call(conn(:get, "/"))
+        end)
 
       assert Regex.match?(~r/\[info\]  GET \//u, log)
       assert Regex.match?(~r/Sent 200 in [0-9]+[µm]s/u, log)
     end
 
     test "it allows logging when the level is not the designated one" do
-      log = capture_log_lines fn ->
-        debug_call(conn(:get, "/health-check"))
-      end
+      log =
+        capture_log_lines(fn ->
+          debug_call(conn(:get, "/health-check"))
+        end)
 
       assert Regex.match?(~r/\[debug\] GET \/health-check/u, log)
       assert Regex.match?(~r/Sent 200 in [0-9]+[µm]s/u, log)
     end
 
     test "it suppresses logging with a custom request path" do
-      log = capture_log_lines fn ->
-        custom_path_call(conn(:get, "/api/status"))
-      end
+      log =
+        capture_log_lines(fn ->
+          custom_path_call(conn(:get, "/api/status"))
+        end)
 
       assert log == ""
     end
 
     test "it suppresses logging for multiple path" do
-      log = capture_log_lines fn ->
-        multi_path_call(conn(:get, "/api/status"))
-      end
+      log =
+        capture_log_lines(fn ->
+          multi_path_call(conn(:get, "/api/status"))
+        end)
+
       assert log == ""
 
-      log = capture_log_lines fn ->
-        multi_path_call(conn(:get, "/api/health"))
-      end
+      log =
+        capture_log_lines(fn ->
+          multi_path_call(conn(:get, "/api/health"))
+        end)
+
       assert log == ""
 
-      log = capture_log_lines fn ->
-        multi_path_call(conn(:get, "/api/something"))
-      end
+      log =
+        capture_log_lines(fn ->
+          multi_path_call(conn(:get, "/api/something"))
+        end)
+
       assert Regex.match?(~r/api\/something/, log)
     end
   end
